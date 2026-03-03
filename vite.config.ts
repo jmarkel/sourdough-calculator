28843e082c3105e88c96c4d01d9d1d18f6bd3c23
Jeff Markel (he/him) <jmarkel1@its.jnj.com>
2026-03-03 10:06:05 -0500

    Set for use in Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/sourdough-calculator/" : "/",
   plugins: [tailwindcss(), react()],
-});
+}));
